4375a7ec75b0da1eac80beeda4b279c5f2a947c8
alex huang <huanglailei@gmail.fake>
2013-07-12 10:59:45 +1000

    add submit text and dsiplay text in the textarea

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,35 +2,36 @@
  * Request handler module, called by router module
  */
 
-var exec = require("child_process").exec;
+//var exec = require("child_process").exec;
+var querystring = require("querystring");
 
 function start(response) {
 	console.log("Request handler 'start' was called.");
-	var content = "empty";
-
-	exec("find /",{timeout:10000,maxBuffer:20000*1024}, function(error, stdout, stderr){
-		//content = stdout;
-		response.writeHead(200,{"Content-Type":"text/plain"});
-		response.write(stdout);
-		response.end();
-	});
 	
-	return content;
+	var body = '<html>'+
+		'<head>'+
+		'<meta http-equiv="Content-Type" content="text/html; '+
+		'charset=UTF-8" />'+
+		'</head>'+
+		'<body>'+
+		'<form action="/upload" method="post">'+
+		'<textarea name="text" rows="20" cols="60"></textarea>'+
+		'<input type="submit" value="Submit text" />'+
+		'</form>'+
+		'</body>'+
+		'</html>';
 
-	/*function sleep(milliSeconds){
-		var startTime = new Date().getTime();
-		while(new Date().getTime() < startTime + milliSeconds);
-	}
-
-	sleep(10000);
-	return "Hello Start";*/
+	response.writeHead(200,{"Content-Type":"text/html"});
+	response.write(body);
+	response.end();
 }
 
-function upload(response) {
+function upload(response,postData) {
 	console.log("Request handler 'upload' was called.");
 	
 	response.writeHead(200,{"Content-Type":"text/plain"});
-	response.write("Hello Upload");
+	response.write("You've sent: "+
+	querystring.parse(postData).text);
 	response.end();
 	
 	//return "Hello Upload";
--- server.js
+++ server.js
@@ -8,14 +8,23 @@ var url = require("url");
 // The main starting point to be called in the terminal
 function start(route, handle){
 	function onRequest(request, response){
+		var postData = "";
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for "+pathname+" received.");
 		
-		/*response.writeHead(200,{"Content-Type":"text/plain"});
-		var content = route(handle,pathname);
-		response.write(content);
-		response.end();*/
-		route(handle,pathname,response);
+		request.setEncoding("utf8");
+
+		request.addListener("data",function(postDataChunk) {
+			postData += postDataChunk;
+			console.log("Received POST data chunk '"+
+			postDataChunk+"'.");
+		});
+
+		request.addListener("end",function(){
+			route(handle,pathname,response,postData);
+		});
+		
+		//route(handle,pathname,response);
 	}
 
 	http.createServer(onRequest).listen(8888);
